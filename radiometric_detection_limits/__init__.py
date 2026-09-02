e16bb2067dcc04b210900e14fb40335b9973dac2
nick-ikon04 <a.galstyan1999@gmail.com>
2026-09-02 07:42:57 +0300

    Add package exports

---
+++ radiometric_detection_limits/__init__.py
@@ -0,0 +1,21 @@
+"""Radiometric decision-threshold and detection-limit calculations."""
+
+from .calculations import (
+    PeakROIResult,
+    RadiometryResult,
+    calc_peak_roi,
+    calc_radiometry,
+    normal_quantile,
+)
+
+__all__ = [
+    "PeakROIResult",
+    "RadiometryResult",
+    "calc_peak_roi",
+    "calc_radiometry",
+    "normal_quantile",
+]
+
+__version__ = "1.0.0"
+
+
